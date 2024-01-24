9fe87c6beee364ebe9a0ab80788452d1e65f9d93
loganamcnichols <131667269+loganamcnichols@users.noreply.github.com>
2024-01-24 14:23:03 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # Replace the following with your API credentials, and rename this file to `config.py`
-OPENAI_API_KEY = "YOUR_OPEN_AI_API_KEY_HERE"
-MJ_API_KEY = "YOUR_MIDJOURNEY_API_KEY_HERE"
+import os
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+MJ_API_KEY = os.environ.get("MJ_API_KEY")
